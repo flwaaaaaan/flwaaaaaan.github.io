1a8ee1df85a2fd3df8ed0cc4a224e93be0feaf0c
fncloud <junkaiw720@gmail.com>
2025-08-11 02:29:53 +0800

    Site updated: 2025-08-11 02:29:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["fncloud/常见问题/","fncloud/全球区苹果ID & 全球软件账号/","fncloud/FnCloud VPN蜂鸟云机场订阅/","fncloud/科学上网后有哪些乐趣/"];function toRandomPost(){
+var posts=["fncloud/科学上网后有哪些乐趣/","fncloud/FnCloud VPN蜂鸟云机场订阅/","fncloud/全球区苹果ID & 全球软件账号/","fncloud/常见问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
