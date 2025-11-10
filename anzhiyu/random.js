aeace523c185ad180f169e456f977b7a5aff4e32
flwaaaaaan <junkaiw720@gmail.com>
2025-11-11 01:58:05 +0800

    Site updated: 2025-11-11 01:58:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=[];function toRandomPost(){
+var posts=["fncloud/科学上网后有哪些乐趣/","fncloud/FnCloud VPN蜂鸟云机场订阅/","fncloud/全球区苹果ID & 全球软件账号/","fncloud/常见问题汇总FAQ/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
